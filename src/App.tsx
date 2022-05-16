5e24e0a6a39258f5d72727e031a5bbbe464ccc50
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-05-16 23:51:14 +0300

    1-Components & props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from "./Todolist";
+
+export type TaskType = {
+  id:number
+  title: string
+  isDone: boolean
+}
 
 function App() {
+  const tasks1: Array<TaskType> = [
+    {id: 1, title: "HTML&CSS", isDone: true},
+    {id: 2, title: "JS", isDone: true},
+    {id: 3, title: "ReactJS", isDone: false}
+  ]
+  const tasks2: Array<TaskType> = [
+    {id: 1, title: "NodeJS world", isDone: true},
+    {id: 2, title: "Express", isDone: false},
+    {id: 3, title: "NestJS", isDone: false}
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Todolist title={"Front-end"} tasks={tasks1}/>
+        <Todolist title={"Back-end"} tasks={tasks2}/>
+      </div>
   );
 }
 
 export default App;
+
