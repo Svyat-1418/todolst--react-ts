c7bf08724a11707a0270bbe51bd9e12b10361acf
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-06-21 23:36:34 +0300

    refactor: AddItemForm, EditableSpan components

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -1,53 +1,52 @@
-import React, {ChangeEvent, KeyboardEvent, useCallback, useState} from "react";
+import {ChangeEvent, FC, KeyboardEvent, memo, useCallback, useState} from "react";
 import IconButton from "@mui/material/IconButton";
 import TextField from "@mui/material/TextField";
 import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
 
 type PropsType = {
-    addItem: (title: string) => void
-    disabled?: boolean
+  addItem: (title: string) => void
+  disabled?: boolean
 }
-export const AddItemForm = React.memo(({addItem, disabled}: PropsType) => {
-    console.log("AddItemForm")
-
-    const [title, setTitle] = useState<string>("")
-    const [error, setError] = useState<string | null>(null)
-
-    const onClickHandler = useCallback(() => {
-        if (title.trim() !== "") {
-            addItem(title.trim())
-            setTitle("")
-        } else {
-            setError("Title is required")
-        }
-    }, [addItem, title])
-    const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.currentTarget.value)
-    }, [])
-    const onKeyDownHandler = useCallback((e: KeyboardEvent<HTMLInputElement>) => {
-        if (error !== null) setError(null)
-        if (e.key === "Enter") {
-            onClickHandler()
-        }
-    }, [onClickHandler, error])
-
-    return (
-        <div>
-            <TextField
-                disabled={disabled}
-                variant={"standard"}
-                error={!!error}
-                helperText={error}
-                label={"Title"}
-                placeholder={"Type title here..."}
-                value={title}
-                onChange={onChangeHandler}
-                onKeyDown={onKeyDownHandler}
-                /*I replace onKeyPress with onKeyDown because onKeyPress deprecated */
-            />
-            <IconButton disabled={disabled} onClick={onClickHandler}>
-                <AddCircleOutline color={"primary"}/>
-            </IconButton>
-        </div>
-    )
+export const AddItemForm: FC<PropsType> = memo(({addItem, disabled}) => {
+  const [title, setTitle] = useState<string>("")
+  const [error, setError] = useState<string | null>(null)
+  
+  const handleAddItem = useCallback(() => {
+    if (title.trim() !== "") {
+      addItem(title.trim())
+      setTitle("")
+    } else {
+      setError("Title is required")
+    }
+  }, [addItem, title])
+  
+  const handleChangeText = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value)
+  }, [])
+  
+  const handleEnterPress = useCallback((e: KeyboardEvent<HTMLInputElement>) => {
+    if (error !== null) setError(null)
+    if (e.key === "Enter") {
+      handleAddItem()
+    }
+  }, [handleAddItem, error])
+  
+  return (
+    <div>
+      <TextField
+        disabled={disabled}
+        variant={"standard"}
+        error={!!error}
+        helperText={error}
+        label={"Title"}
+        placeholder={"Type title here..."}
+        value={title}
+        onChange={handleChangeText}
+        onKeyDown={handleEnterPress}
+      />
+      <IconButton disabled={disabled} onClick={handleAddItem}>
+        <AddCircleOutline color={"primary"}/>
+      </IconButton>
+    </div>
+  )
 })
--- src/components/EditableSpan/EditableSpan.tsx
+++ src/components/EditableSpan/EditableSpan.tsx
@@ -1,42 +1,41 @@
-import React, {ChangeEvent, useCallback, useState} from "react";
+import {ChangeEvent, FC, memo, useCallback, useState} from "react";
 import TextField from "@mui/material/TextField";
 
 type PropsType = {
-    title: string
-    changeTitle: (title: string) => void
-    disabled?: boolean
+  title: string
+  changeTitle: (title: string) => void
+  disabled?: boolean
 }
-export const EditableSpan = React.memo(({title, changeTitle, disabled}: PropsType) => {
-    console.log("EditableSpan")
-
-    const [editMode, setEditMode] = useState<boolean>(false)
-    const [localTitle, setLocalTitle] = useState<string>("false")
-
-    const activateEditMode = () => {
-        if (disabled) {
-            return
-        } else {
-            setLocalTitle(title)
-            setEditMode(true)
-        }
+export const EditableSpan: FC<PropsType> = memo(({title, changeTitle, disabled}) => {
+  const [editMode, setEditMode] = useState<boolean>(false)
+  const [localTitle, setLocalTitle] = useState<string>("false")
+  
+  const activateEditMode = () => {
+    if (disabled) {
+      return
+    } else {
+      setLocalTitle(title)
+      setEditMode(true)
     }
-    const activateViewMode = useCallback(() => {
-        changeTitle(localTitle)
-        setEditMode(false)
-    }, [changeTitle, localTitle])
-    const onChangeHandler = useCallback((e:ChangeEvent<HTMLInputElement>) => {
-        setLocalTitle(e.currentTarget.value)
-    }, [])
-
-    return editMode
-        ? <TextField
-            variant={"standard"}
-            value={localTitle}
-            onChange={onChangeHandler}
-            onBlur={activateViewMode}
-            label={"Edit Title"}
-            autoFocus
-        />
-        : <span onDoubleClick={activateEditMode}>{title}</span>
-
+  }
+  
+  const activateViewMode = useCallback(() => {
+    changeTitle(localTitle)
+    setEditMode(false)
+  }, [changeTitle, localTitle])
+  
+  const handleChangeText = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setLocalTitle(e.currentTarget.value)
+  }, [])
+  
+  return editMode
+    ? <TextField variant={"standard"}
+                 value={localTitle}
+                 onChange={handleChangeText}
+                 onBlur={activateViewMode}
+                 label={"Edit Title"}
+                 autoFocus
+    />
+    : <span onDoubleClick={activateEditMode}>{title}</span>
+  
 })
