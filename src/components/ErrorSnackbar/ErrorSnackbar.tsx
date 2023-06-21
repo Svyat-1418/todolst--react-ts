7587512b6d08fadc4186ac16a018006a81b7dd4e
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-06-22 00:12:29 +0300

    refactor: ErrorSnackbar component

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,31 +1,31 @@
-import React from 'react'
 import Snackbar from '@mui/material/Snackbar'
 import MuiAlert, {AlertProps} from '@mui/material/Alert'
+import {FC, forwardRef, SyntheticEvent} from "react";
 import {useSelector} from "react-redux";
 import {selectAppError} from "../../App/app.selectors";
-import {AppRootStateType, useAppDispatch} from "../../App/store";
 import {setAppError} from "../../App/appReducer";
+import {useAppDispatch} from "../../common/hooks/useAppDispatch";
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-    props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />
+const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />
 })
 
-export const ErrorSnackbar = () => {
-    const error = useSelector(selectAppError)
-    const dispatch = useAppDispatch();
-
-    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-        if (reason === 'clickaway') {
-            return
-        }
-        dispatch(setAppError({error: null}))
+export const ErrorSnackbar: FC = () => {
+  const error = useSelector(selectAppError)
+  const dispatch = useAppDispatch();
+  
+  const handleClose = (event?: SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return
     }
-    return (
-        <Snackbar open={error !== null} autoHideDuration={6000} onClose={handleClose}>
-            <Alert onClose={handleClose} severity='error' sx={{width: '100%'}}>
-                {error}
-            </Alert>
-        </Snackbar>
-    )
+    dispatch(setAppError({error: null}))
+  }
+  return (
+    <Snackbar open={error !== null} autoHideDuration={6000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity='error' sx={{width: '100%'}}>
+        {error}
+      </Alert>
+    </Snackbar>
+  )
 }
